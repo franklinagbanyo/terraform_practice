3d67c9a101212fd8bb8a98c7279e5b08ae985b3c
franklinagbanyo <senyoagbanyo@gmail.com>
2023-07-19 08:48:28 -0600

    ch

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+my_region     = "us-east-1"
+my_profile    = "default"
+topic_name    = "sns-dev"
+team          = "Dev"
+protocol_type = "email"
+endpoint      = "segjgjynfnf@gmail.com"
+
---
+++ variable.tf
@@ -0,0 +1,25 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "my_profile" {
+  default = "profile"
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+}
+
+variable "team" {
+  default = "Dev"
+}
+
+variable "protocol_type" {
+  default = "email"
+}
+
+variable "endpoint" {
+  default = "segjgjynfnf@gmail.com"
+}
+
